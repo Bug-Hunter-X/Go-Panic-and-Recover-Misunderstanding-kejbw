3f37e3ad10fce22d5c0d21686e6ecad67c3367c3
BugHunter-X <askzenai@gmail.com>
2025-01-06 04:30:40 +0100

    Add bug.go file

---
+++ bug.go
@@ -0,0 +1,21 @@
+func main() {
+
+
+
+        fmt.Println("Before the panic")
+
+        defer func() {
+
+                if r := recover(); r != nil {
+
+                        fmt.Println("Recovered from panic:", r)
+
+                }
+
+        }()
+
+        panic("This is a panic")
+
+        fmt.Println("After the panic")
+
+}
